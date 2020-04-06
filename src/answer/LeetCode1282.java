b09cfaaf2bf11fcc3ead57deefc0b9d03c234678
CCCLL <478413408@qq.com>
2020-04-06 23:40:43 +0800

    4.6

---
+++ src/answer/LeetCode1282.java
@@ -0,0 +1,30 @@
+package answer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LeetCode1282 {
+    public static void main(String[] args) {
+        groupThePeople(new int[]{3,3,3,3,3,1,3});
+    }
+    public static List<List<Integer>> groupThePeople(int[] groupSizes) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<List<Integer>> resList = new ArrayList<>();
+        for (int i = 0; i < groupSizes.length; i++) {
+            if (!map.containsKey(groupSizes[i])) {
+                map.put(groupSizes[i], new ArrayList<>());
+            } else if (map.get(groupSizes[i]).size() == groupSizes[i]) {
+                resList.add(map.remove(groupSizes[i]));
+                map.put(groupSizes[i], new ArrayList<>());
+            }
+            map.get(groupSizes[i]).add(i);
+            }
+        for (Integer integer : map.keySet()) {
+            resList.add(map.get(integer));
+        }
+
+        return resList;
+    }
+}
