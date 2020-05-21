4c42e2d80a6cb739bd073fb43c6cd1e5ce736ad6
CCCLL <478413408@qq.com>
2020-05-21 18:33:58 +0800

    5.21

--- src/answer/LeetCode1379.java
+++ src/answer/LeetCode1379.java
@@ -1,29 +1,39 @@
 package answer;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class LeetCode1379 {
-//    public class TreeNode {
-//        int val;
-//        TreeNode left;
-//        TreeNode right;
-//
-//        TreeNode(int x) {
-//            val = x;
-//        }
-//    }
-//    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-//
-//
-//
-//    }
-//    private final TreeNode search(TreeNode cloned,TreeNode target){
-//        if (cloned.val==target.val || cloned==null){
-//            return cloned;
-//        }
-//        TreeNode t=search(cloned.left,target);
-//        if (t==null){
-//            search(cloned.right,target);
-//        };
-//
-//
-//    }
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original==null || (original.val==target.val&&cloned.val==target.val)){
+            return cloned;
+        }
+        TreeNode leftTreeNode = getTargetCopy(original.left, cloned.left, target);
+        TreeNode rightTreeNode = getTargetCopy(original.right, cloned.right, target);
+        return leftTreeNode!=null ? leftTreeNode:rightTreeNode;
+    }
+    public final TreeNode getTargetCopy1(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(original);
+        queue.offer(cloned);
+        while (!queue.isEmpty()){
+            TreeNode  originalTreeNodequeue = queue.poll();
+            TreeNode  clonedTreeNodequeue = queue.poll();
+            if (originalTreeNodequeue.val==target.val&&clonedTreeNodequeue.val==target.val){
+                return clonedTreeNodequeue;
+            }
+            if (originalTreeNodequeue.left!=null){
+                queue.offer(originalTreeNodequeue.left);
+                queue.offer(clonedTreeNodequeue.left);
+            }
+            if (originalTreeNodequeue.right!=null){
+                queue.offer(originalTreeNodequeue.right);
+                queue.offer(clonedTreeNodequeue.right);
+            }
+
+        }
+        return null;
+    }
+
+
 }
