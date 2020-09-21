143684f80cc07844c38c8aeb90ecf1346e64c9cc
CCCLL <478413408@qq.com>
2020-09-21 20:40:19 +0800

    9.21

--- src/answer/LeetCode206.java
+++ src/answer/LeetCode206.java
@@ -5,18 +5,14 @@ package answer;
 public class LeetCode206 {
     public ListNode reverseList(ListNode head) {
         if (head==null)
-            return null;
-        ListNode p=null;
-        ListNode q=head.next;
-        while (q!=null){
-            head.next=p;
-            p=head;
-            head=q;
-            q=q.next;
+            return head;
+        ListNode pre=null;
+        while (head!=null){
+            ListNode next=head.next;
+            head.next=pre;
+            pre=head;
+            head=next;
         }
-        head.next=p;
-        return head;
-
-
+        return pre;
     }
 }
